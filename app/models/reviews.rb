cb5e3d247b6984e049a8432319da180a172438df
Hannah Lee <leexhannah@gmail.com>
2018-04-20 09:16:04 -0700

    pass spec to validate content body and rating for reviews table.

--- app/models/reviews.rb
+++ app/models/reviews.rb
@@ -1,3 +1,5 @@
 class Reviews < ActiveRecord::Base
   validates :author, :presence => true
+  validates :content_body, :presence => true
+  validates :rating, :presence => true
 end
--- spec/models/reviews_spec.rb
+++ spec/models/reviews_spec.rb
@@ -2,5 +2,6 @@ require 'rails_helper'
 
 describe Reviews do
   it { should validate_presence_of :author }
-
+  it { should validate_presence_of :content_body }
+  it { should validate_presence_of :rating }
 end
