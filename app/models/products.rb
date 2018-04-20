c83408a9d77cf0aeb7f51f17586cc8aca2ab7382
Hannah Lee <leexhannah@gmail.com>
2018-04-20 09:05:19 -0700

    fail spec to validate country of origin for products table.

--- app/models/products.rb
+++ app/models/products.rb
@@ -1,4 +1,4 @@
 class Products < ActiveRecord::Base
   validates :name, :presence => true
-
+  validates :cost, :presence => true
 end
--- spec/models/products_spec.rb
+++ spec/models/products_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 describe Products do
   it { should validate_presence_of :name }
   it { should validate_presence_of :cost }
+  it { should validate_presence_of :country_of_origin }
 end
