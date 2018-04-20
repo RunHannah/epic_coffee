a41e983eedb95ab8f9817e2f802fc3422e07a2ca
Hannah Lee <leexhannah@gmail.com>
2018-04-20 10:41:39 -0700

    add create, edit, and update actions to products controller, create corresponding files and errors file.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -13,4 +13,30 @@ class ProductsController < ApplicationController
     @product = Product.new
     render :new
   end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path
+    else
+      render :new
+  end
+
+  def edit
+    @product = Product.find(params[:id])
+    render :edit
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    if @product.update(product_params)
+      redirect_to products_path
+    else
+      render :edit
+    end
+  end
+
+private
+  def product_params
+    params.require(:product).permit(:name)
 end
