2e57b8e447c2b7e2abf1b7c7c7c064ed68c0ae7e
Hannah Lee <leexhannah@gmail.com>
2018-04-20 09:03:54 -0700

    fail spec to validate cost for products table.

--- app/models/products.rb
+++ app/models/products.rb
@@ -1,3 +1,4 @@
 class Products < ActiveRecord::Base
   validates :name, :presence => true
+
 end
--- spec/models/products_spec.rb
+++ spec/models/products_spec.rb
@@ -2,4 +2,5 @@ require 'rails_helper'
 
 describe Products do
   it { should validate_presence_of :name }
+  it { should validate_presence_of :cost }
 end
