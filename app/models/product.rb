21cb1d73c32c913e2032f3d39368cd9688ece3d4
Hannah Lee <leexhannah@gmail.com>
2018-04-20 11:24:03 -0700

    pass spec to have_many reviews.

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,5 @@ class Product < ActiveRecord::Base
   validates :name, :presence => true
   validates :cost, :presence => true
   validates :country_of_origin, :presence => true
-  # has_many :reviews
+  has_many :reviews
 end
