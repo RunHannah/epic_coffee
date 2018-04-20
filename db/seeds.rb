9cf25367ad0d270484efdecb73e7d70cb886ef85
Hannah Lee <leexhannah@gmail.com>
2018-04-20 14:48:41 -0700

    update show page with titles, add edit path, and refactor seeds file to render reviews.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,18 @@
 Product.destroy_all
 Review.destroy_all
 
-10.times do
-  tempProduct = Product.create!(name: Faker::Lorem.sentence(2, false, 0),
+25.times do |index|
+  Product.create!(name: Faker::Lorem.sentence(2, false, 0),
                         cost: (0 + Random.rand(50)),
                         country_of_origin: Faker::Lorem.sentence(3, false, 0))
 
-    (Random.rand(5)).times do
+  @product_id = Product.last.id
+
+  (Random.rand(5)).times do |t|
       Review.create!(author: Faker::Lorem.word,
                       content_body: Faker::Lorem.paragraph,
-                      rating: (1 + Random.rand(5)))
+                      rating: (1 + Random.rand(5)),
+                      product_id: @product_id)
     end
 
 end
