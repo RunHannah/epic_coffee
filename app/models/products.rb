79cf60d9047e310defdd7f4f397766f4a40a139e
Hannah Lee <leexhannah@gmail.com>
2018-04-20 09:48:04 -0700

    add products to routes.rb, create products controller file to render index.

--- app/models/products.rb
+++ app/models/products.rb
@@ -2,5 +2,5 @@ class Products < ActiveRecord::Base
   validates :name, :presence => true
   validates :cost, :presence => true
   validates :country_of_origin, :presence => true
-  # validates :has_many :reviews
+  # has_many :reviews
 end
