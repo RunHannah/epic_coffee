c51f8f82b9ebf39a97afbfc5acf08c689d87321a
Hannah Lee <leexhannah@gmail.com>
2018-04-20 14:34:03 -0700

    refactor products index to have link to details show.html file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,10 @@ Review.destroy_all
                         cost: (0 + Random.rand(50)),
                         country_of_origin: Faker::Lorem.sentence(3, false, 0))
 
-    # (Random.rand(5)).times do
-    #   Review.create!(author: Faker::Types.complex_hash,
-    #   content_body: Faker::Types.string,
-    #   rating: (1 + Random.rand(5)))
-    # end
+    (Random.rand(5)).times do
+      Review.create!(author: Faker::Lorem.word,
+                      content_body: Faker::Lorem.paragraph,
+                      rating: (1 + Random.rand(5)))
+    end
 
 end
