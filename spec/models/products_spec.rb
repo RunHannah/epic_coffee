9f5dbc8c01f22bf67a80ea60e62897081b7b7f56
Hannah Lee <leexhannah@gmail.com>
2018-04-20 09:29:12 -0700

    fail spec to validate many reviews for products.

--- spec/models/products_spec.rb
+++ spec/models/products_spec.rb
@@ -4,4 +4,5 @@ describe Products do
   it { should validate_presence_of :name }
   it { should validate_presence_of :cost }
   it { should validate_presence_of :country_of_origin }
+  it { should have_many :reviews }
 end
