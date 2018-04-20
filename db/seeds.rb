c46f883fa45404585a11c34e57407ef78be5f95d
Hannah Lee <leexhannah@gmail.com>
2018-04-20 14:58:40 -0700

    update seed to use multiple fakers.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,16 +1,16 @@
 Product.destroy_all
 Review.destroy_all
 
-25.times do |index|
-  Product.create!(name: Faker::Lorem.sentence(2, false, 0),
+50.times do |index|
+  Product.create!(name: Faker::Coffee.blend_name,
                         cost: (0 + Random.rand(50)),
-                        country_of_origin: Faker::Lorem.sentence(3, false, 0))
+                        country_of_origin: Faker::Address.country)
 
   @product_id = Product.last.id
 
-  (Random.rand(5)).times do |t|
-      Review.create!(author: Faker::Lorem.word,
-                      content_body: Faker::Lorem.paragraph,
+  (Random.rand(250)).times do |t|
+      Review.create!(author: Faker::Kpop.solo,
+                      content_body: Faker::Lorem.sentence(50, false, 250),
                       rating: (1 + Random.rand(5)),
                       product_id: @product_id)
     end
