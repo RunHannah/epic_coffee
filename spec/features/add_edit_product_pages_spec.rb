358d28fa8e859879b178359b35f527cccb071135
Guest User <Guest@Epicodus-3E.local>
2018-04-26 19:15:53 -0700

    add edit product pages spec and passing test.

--- spec/features/add_edit_product_pages_spec.rb
+++ spec/features/add_edit_product_pages_spec.rb
@@ -1,14 +1,14 @@
 require 'rails_helper'
 
-describe "adding a review" do
-  it "adds a review to a coffee blend" do
+describe "editing a product" do
+  it "edits fields for coffee blend" do
     product = Product.create(name: 'Strong Breaker', cost: 10, country_of_origin: 'Brunei Darussalam')
     visit edit_product_path(product)
-    fill_in 'Name', :with => 'Coffee for Life'
-    fill_in 'Cost', :with => 40
+    fill_in 'Name', :with => 'Black Coffee Forever'
+    fill_in 'Cost', :with => 35
     fill_in 'Country of origin', :with => 'Korea'
     click_button 'Update Product'
-    expect(page).to have_content 'Coffee for Life'
+    expect(page).to have_content 'Black Coffee Forever'
   end
 
 end
