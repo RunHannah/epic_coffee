02042d0c89d9aa0d1b681d2eaf7fd1fed9c3c549
Guest User <Guest@Epicodus-3E.local>
2018-04-26 19:35:15 -0700

    fail spec to test presence of rating value under reviews.

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,7 @@ class Review < ActiveRecord::Base
   validates :author, :presence => true
   validates :content_body, :presence => true
   validates :rating, :presence => true
+  # validates :rating, :inclusion => 1..5
 
   belongs_to :product
 end
